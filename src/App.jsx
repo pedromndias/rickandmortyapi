4c7227dd8b51e19c0669c34eac030796b76d43b3
Pedro Dias <pedrodias@Pedros-MacBook-Pro-3.local>
2023-11-05 21:27:18 +0100

    search feature done

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,98 @@
-import logo from './assets/logo.png'
-import './App.css'
-import axios from 'axios'
-import { useEffect, useState } from 'react'
+import logo from "./assets/logo.png";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import DotLoader from "react-spinners/DotLoader";
-import Character from './components/Character';
+import Character from "./components/Character";
 
 function App() {
-
   // Let's create a state so we manage the characters:
-  const [characters, setCharacters] = useState(null)
+  const [characters, setCharacters] = useState(null);
+  // Let's create a variable for the search word:
+  const [query, setQuery] = useState("");
+  // Create another state to manage the loading:
+  const [isLoading, setIsLoading] = useState(true);
+  // Create another state to manage the initial loading:
+  const [isLoadingInitial, setIsLoadingInitial] = useState(true);
+  // Set a state if there is a character or not:
+  const [characterExists, setCharacterExists] = useState(true);
 
   // Now let's get the data from the api. It will be an asynchronous process so we can use an async function. To handle the api call we use a try-catch.
   const getData = async () => {
     try {
-      const response = await axios.get("https://rickandmortyapi.com/api/character")
-      // console.log(response.data)
-      setCharacters(response.data.results)
+      const response = await axios.get(
+        "https://rickandmortyapi.com/api/character"
+      );
+      // console.log(response.data);
+      setCharacters(response.data.results);
+      setIsLoadingInitial(false);
     } catch (error) {
-      console.log(error)
+      // console.log(error);
     }
-  }
+  };
+
+  // Let's create another function for the search functionality:
+  const getQuery = async () => {
+    try {
+      setIsLoading(true);
+      const searchResponse = await axios.get(
+        `https://rickandmortyapi.com/api/character/?name=${query}`
+      );
+      // console.log(searchResponse.data);
+      setCharacters(searchResponse.data.results);
+      setCharacterExists(true);
+      // console.log(characters);
+      setIsLoading(false);
+    } catch (error) {
+      // console.log(error);
+      // If we can't find a character, we will set the characterExists to false:
+      setCharacterExists(false);
+      setIsLoading(false);
+    }
+  };
 
   // We will use a useEffect to getData is invoked when the App component is mounted:
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
+
+  // And another useEffect for the search feature:
+  useEffect(() => {
+    getQuery();
+  }, [query]);
 
   // Let's create a clause so we have a spinner while the data is loading:
-  if (characters === null) {
-    return <DotLoader color="#36d7b7" size={60}/>
+  if (isLoadingInitial) {
+    return <DotLoader color="#36d7b7" size={60} />;
   }
 
   return (
-    <>
-      <div>
-        
+    <div className="main-container">
+      <section>
         <img src={logo} className="logo" alt="logo" />
-        <div className='characters-list'>
-          {characters.map(eachCharacter => <Character character={eachCharacter} key={eachCharacter.id}/>)}
+        <div className="search-container">
+          <label htmlFor="search-char">Search for a Character: </label>
+          <input
+            type="text"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
         </div>
-      </div>
-    </>
-  )
+        {characterExists === false ? (
+          <p style={{color: "red"}}>Can't find a character!</p>
+        ) : (
+          <div className="characters-list">
+            {characters.map((eachCharacter) => (
+              <Character character={eachCharacter} key={eachCharacter.id} />
+            ))}
+          </div>
+        )}
+      </section>
+      <footer>
+        <p>Made with ♥︎ by Pedro</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,9 +1,14 @@
+import locationLogo from "../assets/location-icon.png"
+
 const Character = (props) => {
     // Let's destructure the props:
     const {character} = props
   return (
     <div className="each-character">
+        <img src={character.image} alt={character.name} />
         <h3>{character.name}</h3>
+        <p>👤 {character.species}</p>
+        <p id="location"><img src={locationLogo} alt="location" /> {character.location.name}</p>
     </div>
   )
 }
